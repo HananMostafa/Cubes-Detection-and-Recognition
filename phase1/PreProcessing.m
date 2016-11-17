5adc3e664b62cdd0b24fe54d0dee8da93e8fe65d
HananMostafa <hananmostafa895@gmail.com>
2016-11-17 15:04:02 +0200

    no change

--- phase1/PreProcessing.m
+++ phase1/PreProcessing.m
@@ -3,4 +3,6 @@ function enhancedImage = PreProcessing(inputImage)
 %apply preprocessing steps on the inputImage and return enhancedImage
 %READABLE and COMMENTED please!
 
+% msh hnn7tag preprocessing 
+
 end
